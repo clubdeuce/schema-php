14b8f0220629a6efeb5761e06162b90f94dd0a27
Daryl <daryl@clubdeuce.net>
2025-08-25 20:36:15 -0400

    Refactor Organization class: Add public accessors for address and telephone methods

--- src/Organization.php
+++ src/Organization.php
@@ -6,26 +6,30 @@ namespace Clubdeuce\Schema;
  * Represents an organizational entity extending the Base class.
  *
  * @link https://schema.org/Organization
- *
- * @method PostalAddress|null address()
- * @method string             telephone()
  */
 class Organization extends Thing
 {
     protected ?PostalAddress $_address = null;
     protected string $_telephone = '';
 
-    /**
-     * @return array
-     */
-    function schema(): array
+    public function address(): ?PostalAddress
+    {
+        return $this->_address;
+    }
+
+    public function schema(): array
     {
         $schema = array_merge(parent::schema(), array(
             '@type'     => 'Organization',
-            'address'   => $this->address() ? $this->address()->schema() : null,
+            'address'   => $this->address()?->schema(),
             'telephone' => $this->telephone(),
         ));
 
         return $schema;
+    }
+
+    public function telephone(): string
+    {
+        return $this->_telephone;
     }
 }
